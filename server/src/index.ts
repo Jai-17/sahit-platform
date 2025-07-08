0dc6d9a7a33887121ba7480f90f518534711d435
Jai-17 <madhukarjai9@gmail.com>
2025-07-08 10:53:36 +0530

    init

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({message: "Server is running!"});
+})
+
+const port = Number(process.env.PORT) || 8000;
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${port}`);
+})
