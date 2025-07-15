7d06da26c894c585abdb96684afd3ac7e46f2862
Jai-17 <madhukarjai9@gmail.com>
2025-07-15 16:23:14 +0530

    Update: Matching Logic

--- server/src/utils/worker.ts
+++ server/src/utils/worker.ts
@@ -1,5 +1,6 @@
 import { Queue, Worker } from "bullmq";
 import redis from "./redis";
+import { prisma } from "../db";
 
 export const queue = new Queue("matchRequest", {
     connection: redis
@@ -7,7 +8,53 @@ export const queue = new Queue("matchRequest", {
 
 const worker = new Worker('matchRequest', async job => {
     console.log("From Worker", job.data);
-    const data = job.data;
+    const helpRequestId = job.data.helpRequestId;
+
+    const request = await prisma.helpRequest.findUnique({where: {id: helpRequestId}});
+    if(!request) {
+        console.error("Error no helprequest found with this id", helpRequestId);
+    }
+    console.log("Found Request");
+
+    const requestingUser = await prisma.helpSeeker.findUnique({where: {id: request?.userId}, select: {city: true, state: true}})
+
+    // Can Update Matching Logic
+    const matchingNGOs = await prisma.nGO.findMany({
+        where: {
+            supportTypes: {has: request?.helpType},
+            city: requestingUser?.city,
+            state: requestingUser?.state,
+        },
+        orderBy: {
+            rating: 'desc',
+        },
+        take: 3
+    })
+    console.log("Sending Mails to NGOs");
+
+    // Send Mails to NGO
+    
+    if(matchingNGOs.length > 0) {
+        await prisma.helpRequest.update({
+            where: {id: request?.id},
+            data: {
+                ngoId: matchingNGOs[0].id,
+                requestedNGOs: {
+                    connect: matchingNGOs.map(ngo => ({id: ngo.id}))
+                },
+                status: "SEND_TO_NGOS",
+            }
+        })
+    } else {
+        await prisma.helpRequest.update({
+            where: {id: request?.id},
+            data: {
+                status: "DECLINED_BY_ALL",
+            }
+        })
+    }
+
+    console.log("DATA Updated in DB");
 }, {
     concurrency: 100, connection: redis
 })
